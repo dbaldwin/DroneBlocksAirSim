b60171c37532ad34e02c614ddb040ca6c07e3ac0
Dennis Baldwin <dennisbaldwin@gmail.com>
2020-11-19 06:23:16 -0600

    Communication from webView to native. Need to make this async.

--- DroneBlocksAirSim/MainPage.xaml.cs
+++ DroneBlocksAirSim/MainPage.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using System.Diagnostics;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -32,11 +22,20 @@ namespace DroneBlocksAirSim
 
             client = new TCP();
 
+
+            webView.ScriptNotify += webView_ScriptNotify;
+
+        }
+
+        async void webView_ScriptNotify(object sender, NotifyEventArgs e)
+        {
+            client.Send();
         }
 
         private void TestButton_Click(object sender, RoutedEventArgs e)
         {
             client.Send();
         }
+
     }
 }
