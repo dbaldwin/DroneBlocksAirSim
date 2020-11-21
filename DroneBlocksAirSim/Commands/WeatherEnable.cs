40f2f535ab323a1740b037d05177a634b8a3ce41
Dennis Baldwin <dennisbaldwin@gmail.com>
2020-11-20 18:10:29 -0600

    Weather

---
+++ DroneBlocksAirSim/Commands/WeatherEnable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneBlocksAirSim.Commands
+{
+    
+    class WeatherEnable: MessagePackCommand
+    {
+        private readonly MessagePackCommand command;
+
+        public WeatherEnable(bool enable)
+        {
+            command = new MessagePackCommand
+            {
+                Request = 0,
+                MessageId = 1,
+                Method = "simEnableWeather",
+                args = new ArrayList { enable }
+            };
+        }
+
+        public MessagePackCommand GetCommand()
+        {
+            return command;
+        }
+    }
+}
---
+++ DroneBlocksAirSim/Commands/WeatherSet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneBlocksAirSim.Commands
+{
+    class WeatherSet: MessagePackCommand
+    {
+        private readonly MessagePackCommand command;
+
+        public WeatherSet(int weatherType, float intensity)
+        {
+            command = new MessagePackCommand
+            {
+                Request = 0,
+                MessageId = 0,
+                Method = "simSetWeatherParameter",
+                args = new ArrayList { weatherType, intensity }
+            };
+        }
+
+        public MessagePackCommand GetCommand()
+        {
+            return command;
+        }
+    }
+}
