1211449768ff5fadcbfcc730c61479bb573d354c
Dennis Baldwin <dennisbaldwin@gmail.com>
2020-11-21 07:17:19 -0600

    Arm disarm

---
+++ DroneBlocksAirSim/Commands/ArmDisarm.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+namespace DroneBlocksAirSim.Commands
+{
+    class ArmDisarm
+    {
+
+        private readonly MessagePackCommand command;
+
+        public ArmDisarm(bool arm)
+        {
+            command = new MessagePackCommand
+            {
+                Request = 0,
+                MessageId = 1,
+                Method = "armDisarm",
+                args = new ArrayList { arm, "" }
+            };
+        }
+
+        public MessagePackCommand GetCommand()
+        {
+            return command;
+        }
+    }
+}
